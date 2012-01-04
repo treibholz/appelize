3435c19656165eb3a2ca7247391b0cb041bd4269
Klaus Umbach <klaus@uxix.de>
2012-01-04 12:07:51 +0100

    singlethreaded encoding works now

--- appelize.py
+++ appelize.py
@@ -10,15 +10,21 @@ __revision__ = "$Id$"
 #
 
 import os
+import mutagen
+import thread
+from time import sleep
 
-class musicDirectories(object):
+class musicDirectories(object): # {{{
    """docstring for musicDirectories"""
-   def __init__(self, srcDir, destDir):
+
+   def __init__(self, srcDir, destDir): # {{{
       super(musicDirectories, self).__init__()
 
       self.recodeExtensions = { 'ogg'  : 'mp3',
                                 'flac' :'mp3',
                               }
+      self.maxThreads = 4 
+
       self.recodeList = []
 
       # expand the directories
@@ -32,33 +38,38 @@ class musicDirectories(object):
       findCmd = 'find "%s" -type f' % self.srcDir
       self.fileList = [ x.strip()[len(self.srcDir)+1:] for x in os.popen(findCmd).readlines() ]
 
+   # }}}
 
-   def checkEncode(self,fileName):
+   def checkEncode(self,fileName): # {{{
       """docstring for checkEncode"""
       ext = os.path.splitext(fileName)[1][1:].lower()
       if ext in self.recodeExtensions:
          return ext
       else:
          return False
+   # }}}
 
-   def recode(self,filename):
+   def recode(self,filename): # {{{
       """docstring for recode"""
       self.recodeList.append(filename)
+   # }}}
 
-   def hardLink(self,filename):
+   def hardLink(self,filename): # {{{
       """docstring for hardlink"""
       dest = os.path.join(self.destDir,filename)
       if not os.path.exists(dest):
          src = os.path.join(self.srcDir,filename)
          os.link(src,dest)
+   # }}}
 
-   def mkDestDir(self,filename):
+   def mkDestDir(self,filename): # {{{
       """docstring for mkDestDir"""
       destDir = os.path.join(self.destDir,os.path.dirname(filename))
       if not os.path.exists(destDir):
          os.makedirs(destDir)
+   # }}}
 
-   def easywork(self):
+   def easywork(self): # {{{
       """docstring for work"""
       for i in self.fileList:
          self.mkDestDir(i)
@@ -66,15 +77,85 @@ class musicDirectories(object):
             self.recode(i)
          else:
             self.hardLink(i)
+   # }}}
 
-   def hardwork(self):
+   def hardwork(self): # {{{
       """docstring for hardwork"""
-      print self.recodeList
+      for i in self.recodeList:
+         inFile  = os.path.join(self.srcDir,i)
+         outFile = os.path.join(self.destDir, '%s.mp3' % os.path.splitext(i)[0])
+         self.mkDestDir(i)
+         s = Recode(inFile,outFile)
+         s.work()
+   # }}}
 
+# }}}
+
+class Recode(object): # {{{
+   """docstring for Recode"""
+   def __init__(self, inFile, outFile, outFormat='mp3'): # {{{
+      super(Recode, self).__init__()
+
+      tagTranslate = { # here will be more soon
+         'lame' : {
+            "album"        : '--tl',
+            "artist"       : '--ta',
+            "title"        : '--tt',
+            "date"         : '--ty',
+            "genre"        : '--tg',
+            "tracknumber"  : '--tn'
+         }
+      }
+
+      self.inFile = inFile
+
+      # read the tags from the file
+      tags = mutagen.File(inFile)
+      if tags == None:
+         print "%s has no readable tags"  % (inFile, )
+         self.cmd='/bin/true'
+      else:
+
+         # get the format according to the extension (cheap!)
+         inFormat = os.path.splitext(inFile)[1][1:].lower()
+
+         decoder = {  'flac'   : 'flac --silent --stdout --decode "%s" ' % (inFile, ),
+                           'ogg'    : 'oggdec --quiet --output=- "%s" ' % (inFile, ),
+                        }
+
+         encoder = {  'mp3' :  'lame',
+                           'm4a' :  'faac',
+                        }
+
+         encoderOpts = { 'lame' : '-s 44.1 -q 0 -V 0 -v -b 192 -B 256 --quiet -' }
+
+         tagOptions = ''
+         for i in tags.keys():
+            try:
+               tagOptions += ' %s "%s" ' % ( tagTranslate[encoder[outFormat]][i], tags[i][0], )
+            except KeyError:
+               pass
+         try:
+            self.cmd = u'%s | %s %s %s "%s"' % (decoder[inFormat], encoder[outFormat], tagOptions, encoderOpts[encoder[outFormat]], outFile, )
+         except:
+            print decoder[inFormat]
+            print encoder[outFormat]
+            print tagOptions
+            self.cmd='/bin/true'
+
+   # }}}
+
+   def work(self): # {{{
+      """docstring for work"""
+      print "recoding: %s" % (self.inFile,)
+      os.system(self.cmd)
+   # }}}
+
+# }}}
 
 if __name__ == "__main__":
 
-   m = musicDirectories('~/Musik', '~/applemusic')
+   m = musicDirectories('~/Musik/Tool', '~/applemusic')
 
    m.easywork()
    m.hardwork()
