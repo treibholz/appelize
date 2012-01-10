e895d06eecd167e9ff3740e2e17dfba3bcde585c
Klaus Umbach <klaus-github@uxix.de>
2012-01-10 19:56:06 +0100

    incremented version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup as _setup
 if __name__ == '__main__':
     _setup(
         name = 'appelize',
-        version = '0.3.1',
+        version = '0.3.2',
         description = 'This program "appelizes" your music into a separate directory.',
         author = 'Klaus Umbach',
         author_email = 'klaus-github@uxix.de',
